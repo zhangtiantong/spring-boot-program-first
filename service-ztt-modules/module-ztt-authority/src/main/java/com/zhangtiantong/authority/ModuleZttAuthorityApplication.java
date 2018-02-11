0a8d59ebe9ffb2ee4eec722456f9bac26b081056
zhangtiantong <zhangtiantong@aerozhonghuan.com>
2018-02-11 23:33:30 +0800

    增加认权模块

---
+++ service-ztt-modules/module-ztt-authority/src/main/java/com/zhangtiantong/authority/ModuleZttAuthorityApplication.java
@@ -0,0 +1,12 @@
+package com.zhangtiantong.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleZttAuthorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleZttAuthorityApplication.class, args);
+	}
+}
