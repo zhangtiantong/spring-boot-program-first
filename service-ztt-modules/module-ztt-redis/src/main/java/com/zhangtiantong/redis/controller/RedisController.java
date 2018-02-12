21a71ad28d2b7278866272a99ccf9d1d54b938b5
zhangtiantong <zhangtiantong@aerozhonghuan.com>
2018-02-12 16:21:50 +0800

    增加持久层处理模块

--- service-ztt-modules/module-ztt-redis/src/main/java/com/zhangtiantong/redis/controller/RedisController.java
+++ service-ztt-modules/module-ztt-redis/src/main/java/com/zhangtiantong/redis/controller/RedisController.java
@@ -1,19 +1,49 @@
 package com.zhangtiantong.redis.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.zhangtiantong.redis.entity.User;
+import com.zhangtiantong.redis.service.RedisService;
 import com.zhangtiantong.redis.service.RedisTestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by dell on 2017/10/23.
  */
 
-@Controller
+@RestController
 public class RedisController {
 
     @Autowired
     private RedisTestService redisTestService;
+
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping(value = "/setObjectToRedis", method = {RequestMethod.GET, RequestMethod.POST})
+    public void setObjectToRedis(@RequestBody User user) {
+        redisService.setObj(user.getId(), user);
+    }
+
+    @RequestMapping("/setStrToRedis")
+    public void setStrToRedis(String key, String value) {
+
+        redisService.setStr(key, value);
+    }
+
+    @RequestMapping(value = "/getObjectToRedis", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String getObjectToRedis(@RequestBody User user) {
+
+        Object user1 = redisService.getObj(user.getId());
+        return JSON.toJSONString(user1);
+    }
+
+    @RequestMapping(value = "/getStrToRedis", method = {RequestMethod.GET})
+    @ResponseBody
+    public String getStrToRedis(String key) {
+        return redisService.getStr(key);
+    }
 
     @RequestMapping("/redisTest")
     public void test() {
